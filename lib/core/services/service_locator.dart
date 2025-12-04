19612ee79696165d51de9bf27ca49d3dfc10aa11
aliftheprogramer <alifarya679@gmail.com>
2025-12-05 04:35:50 +0700

    Add authentication use cases and register them in service locator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -2,7 +2,13 @@
 
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rentverse/common/bloc/auth/auth_cubit.dart';
 import 'package:rentverse/core/network/dio_client.dart';
+import 'package:rentverse/features/auth/domain/usecase/get_local_user_usecase.dart';
+import 'package:rentverse/features/auth/domain/usecase/get_user_usecase.dart';
+import 'package:rentverse/features/auth/domain/usecase/is_logged_in_usecase.dart';
+import 'package:rentverse/features/auth/domain/usecase/login_usecase.dart';
+import 'package:rentverse/features/auth/domain/usecase/register_usecase.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final sl = GetIt.instance;
@@ -12,4 +18,12 @@ Future<void> setupServiceLocator() async {
   sl.registerLazySingleton(() => sharedPreferences);
   sl.registerLazySingleton<DioClient>(() => DioClient(sl(), sl()));
   sl.registerLazySingleton<Dio>(() => sl<DioClient>().dio);
+
+  //auth
+  sl.registerLazySingleton(() => GetLocalUserUseCase(sl()));
+  sl.registerLazySingleton(() => IsLoggedInUsecase(sl()));
+  sl.registerLazySingleton(() => LoginUseCase(sl()));
+  sl.registerLazySingleton(() => RegisterUsecase(sl()));
+  sl.registerLazySingleton(() => GetUserUseCase(sl()));
+  sl.registerLazySingleton(() => AuthCubit());
 }
